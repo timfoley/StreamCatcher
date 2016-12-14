5f39313e78bed3e9b16cd8fcced5c573bd54889d
Tim Foley <thetimfoley@gmail.com>
2016-12-14 16:34:17 -0500

    Minor text fixes

--- src/FilterControls.js
+++ src/FilterControls.js
@@ -13,7 +13,7 @@ class FilterControls extends Component {
           <label><input type="checkbox" className="control__input" id="amazon_prime" onChange={e => this.props.onSourceChange(e, this)} checked={this.props.filters.sources.amazon_prime}/> Amazon Prime</label>
           {/* <label><input type="checkbox" className="control__input" id="youtube" onChange={e => this.props.onSourceChange(e, this)} checked={this.props.filters.sources.youtube}/> YouTube</label> */}
         </div>
-        <h2>Rotten Tomatoes Filter:</h2>
+        <h2>Min. Rotten Tomatoes Score:</h2>
         <div className="scores">
           {/* <label>Minimum Rotten Tomatoes Score: <input className="input" required type="number" min="0" max="95" step="5" id="rt" value={this.props.filters.rt} onChange={e => this.props.onScoreChange(e, this)}/></label> */}
           <label><input className="input" required type="range" min="0" max="95" step="5" id="rt" value={this.props.filters.rt} onChange={e => this.props.onScoreChange(e, this)}/><input className="input" required type="number" min="0" max="95" step="5" id="rt" value={this.props.filters.rt} onChange={e => this.props.onScoreChange(e, this)}/></label>
